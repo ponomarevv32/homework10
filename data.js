5eb5859d9f58bfc46a13cb012eb0ae06ab38fde9
ponomarevv32 <ponomarevv3007@gmail.com>
2023-02-25 16:55:27 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,22 @@
+let dataInfo = `{ 
+  "id": "8271",
+  "type": "anime",
+  "attributes": {
+    "synopsis": "Tokyo has become a cruel and merciless city—a place where vicious creatures called “ghouls”...",
+    "titles": {
+      "en": "Tokyo Ghoul",
+      "en_jp": "Tokyo Ghoul",
+      "en_us": "Tokyo Ghoul"
+    },
+    "canonicalTitle": "Tokyo Ghoul"
+  },
+  "favoritesCount": 3599,
+  "popularityRank": 30,
+  "ageRating": "R",
+  "posterImage": {
+    "original": "https://media.kitsu.io/anime/poster_images/8271/original.jpg?1597694836"
+  },
+  "episodeCount": 12,
+  "episodeLength": 24,
+  "totalLength": 288
+}`
